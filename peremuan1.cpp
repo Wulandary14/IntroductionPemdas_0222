d890cd757ee08df10572baf6c45ba6e30ed68bf4
Wulandary14 <Moonywulan14@gmail.com>
2026-02-23 21:45:54 +0700

    menambahkan library

---
+++ peremuan1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//variabel global
+int sks;
+
+int main(){
+    //variabel lokal
+    int umur;
+    umur = 20;
+
+    cout << "Hallo guys" << endl;
+    cout << 10 << endl;
+    cout << umur;
+    
+}
