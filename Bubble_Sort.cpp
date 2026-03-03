c7b722f52176f88816d0d2fe11bd295b98112b29
Wulandary14 <Moonywulan14@gmail.com>
2026-03-03 12:04:20 +0700

    deklarasi variabel

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; //deklarasi variable global array a dengan ukuran 20
+int n; //deklarasi variable global n untuk menyimpan banyaknya elemen pada array
+
